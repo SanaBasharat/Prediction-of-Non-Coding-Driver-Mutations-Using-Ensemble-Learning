17651a17861b75852e159139aea67e17c7a9296f
ramalh <hramal45@gmail.com>
2023-11-18 23:18:12 +0300

    updating new method to find interaction number, but file processing is unchanged

--- Long Range Interactions/interactions_code_multiprocessing.py
+++ Long Range Interactions/interactions_code_multiprocessing.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 import multiprocessing
 import time
-from anytree import Node, RenderTree, AsciiStyle, LevelGroupOrderIter, LevelOrderGroupIter, search
 from intervaltree import Interval, IntervalTree
 import sys
 import yaml
@@ -22,100 +21,148 @@ def read_dataset(filename):
     df = df[['chr', 'start', 'end', 'pos_37', 'driver']]
     return df
 
-def read_file(filename, chr):
-    cp = pd.read_csv('ChIA-PET data/' + filename, sep = '\t',  header = None)
-    cp.columns = ['chr_A', 'start_A', 'end_A', 'chr_B', 'start_B', 'end_B', 'score']
-    cp['chr_A'] = cp['chr_A'].map(lambda x: x.replace('chr', ''))
-    cp['chr_B'] = cp['chr_B'].map(lambda x: x.replace('chr', ''))
+def read_files(filename):
+    # read file
+    interactions = pd.read_csv(f"ChIA-PET data/{filename}.bedpe", delimiter="\t",\
+            names = ["CHR1", "start1", "end1", "CHR2", "start2", "end2", "p2"],\
+            usecols=["CHR1", "start1", "end1", "CHR2", "start2", "end2", "p2"], 
+            header=None, dtype=\
+            {0: object, 1: int, 2: int, 3: object, 4: int, 5: int, 6: int})
     
-    cp = cp[cp['chr_A'] == chr]
-    return cp
+    return interactions
 
-def make_interval_tree(df):
-    interactions_tree = IntervalTree()
 
-    for index, row in df.iterrows():
-        interactions_tree.add(Interval(row['start_A'], row['end_A'], tuple([row['chr_A'], row['start_B'], row['end_B']]))) # fotwards interaction
-        interactions_tree.add(Interval(row['start_B'], row['end_B'], tuple([row['chr_A'], row['start_A'], row['end_A']]))) # backwards interaction
+def filter_bedpe_file(interactions, chromosome):
+    # filter based on chromosome
+    interactions = interactions[ interactions["CHR1"] == chromosome ].copy()
+    interactions.drop(columns=["CHR1", "CHR2"], inplace=True)
+    interactions.reset_index(drop=True, inplace=True)
+    indexStop = interactions.index.stop
     
-    return interactions_tree
+    # add positive index to intervals on left
+    interactions.insert(2, "p1", list(map( lambda x: set([x]),range(1, 1 + indexStop)) ))
+    
+    # add negative index to intervals on right
+    interactions["p2"] = list(map( lambda x: set([x]), range(-1, -1 -1 * indexStop, -1) ))
+    
+    # separate them into different arrays
+    int1 = interactions.iloc[:, :3].values
+    int2 = interactions.iloc[:, 3:].values
+    
+    # merge two array into one array, 0th index will be (0,0,0) array
+    # In each sub array, the rightmost value will be its index as well (including minus index)
+    result_int = np.concatenate(( int1, int2[::-1] ))
+    return result_int, indexStop
 
-def find_interactions(mutation, interactions_tree): # the mutation and interaction tree will be of the same chr
-    df_tree = Node(name = 'artificial', checked = 0)
+
+def create_tree(data):
+    # create IntervalTree from array
+    GenomicTree = IntervalTree( Interval( *j ) \
+            for j in data)
     
-    found_overlap = False
-    if len(interactions_tree.at(mutation)) > 0:
-        found_overlap = True
-        for i in interactions_tree.at(mutation):
-            Node(Interval(i.data[1], i.data[2]), checked = 0, parent = df_tree)
-            try:
-                interactions_tree.remove(i)
-            except:
-                pass
-            try:
-                inv = Interval(i.data[1], i.data[2], tuple([i.data[0], i.begin, i.end]))
-                interactions_tree.remove(inv)
-            except:
-                pass
-       
-    interactions = 0
-    chains = 0
+    return GenomicTree
+
+
+def merge_direct_overlaps(tree, data_reducer = None):
+    if data_reducer == None:
+        data_reducer = lambda *x: None
     
-    if found_overlap:
-        children_left = True
-        while children_left is True:
-            children_left = False
+    sorted_tree = sorted(tree)
+    merged = [sorted_tree[0]]
+    lt = len(sorted_tree)
+    highest = sorted_tree[0][0] - 1 # var for checking if interval merged before
+    
+    i = -1
+    for inv in sorted_tree:
+        i += 1
+        j = i + 1
+        while j < lt and inv.end > sorted_tree[j].begin :
+            upper = max(inv.end, sorted_tree[j].end)
+            data = data_reducer(inv.data, sorted_tree[j].data)
+            merged.append(Interval(inv.begin, upper, data))
+            if inv.end > highest:
+                highest = inv.end
+            j += 1
+        if inv.begin > highest:
+            merged.append(inv)
+    new_tree = IntervalTree(merged[1:])
+    
+    return new_tree
+
+
+def tree_to_array(tree, edge_length):
+    array = np.full( 2*edge_length + 1, set(), dtype=set)
+    
+    for i in tree:
+        for j in i.data:
+            tmp = array[j] | i.data
+            array[j] = tmp.difference([j])
+    
+    return array
+
+
+def build_tree(interactions, GenomicTree_at_mutation):
+    
+    initials = set()
+    for i in GenomicTree_at_mutation:
+        initials.add(int(*i.data))
+    # if root has no descendants, there is no overlap at all
+    if len(initials) == 0:
+        #print("No matching")
+        return 0
+    
+    # assing initial values to length values
+    used_indices = set( initials )
+    indices = set( [ -1 * i for i in used_indices ] )
+    [ used_indices.add(i) for i in indices ]
+    values = set()
+    while True:
+        values = set.union( *interactions[[ *indices ]] )
+        indices = set( [-1 * i for i in values] )
+        indices = indices.difference(used_indices)
+        if not indices:
+            break
+        used_indices = used_indices | indices
         
-            for node in df_tree.leaves:
-                if node.checked == 0:
-                    node.checked = 1
-                    found_list = list(interactions_tree.overlap(node.name))
-                    for found_node in found_list:
-                        children_left = True
-                        Node(name = Interval(found_node.data[1], found_node.data[2]), checked = 0, parent = node)
-                        try:
-                            interactions_tree.remove(found_node)# only remove that node which satisfies both conditions of similarity
-                        except:
-                            pass
-                            # ("Node was already removed")
-                        try:    
-                            interactions_tree.remove(Interval(found_node.data[1], found_node.data[2], tuple([found_node.data[0], found_node.begin, found_node.end])))
-                        except:
-                            pass
-                            # print("Node was already removed")
-        interactions = len(set([k.name for k in df_tree.descendants]))
-        chains = sum(len(x) for x in [leaf for leaf in LevelGroupOrderIter(df_tree, filter_=lambda node: node.is_leaf and not node.is_root) if leaf])
+    return len(used_indices)
 
-    return interactions, chains
-    
-def fill_columns(metadata, file):
-    if metadata[metadata['File accession'] == file.replace('.bedpe', '')]['Experiment target'].iloc[0].replace('-human', '') == 'CTCF':
-        return 'CTCF_interactions', 'CTCF_chains'
-    else:
-        return 'POLR2A_interactions', 'POLR2A_chains'
-    
-def worker(filename, metadata, df):
-    start_time = time.perf_counter()
-    int_col, chains_col = fill_columns(metadata, filename)
+
+def reducer(old, new):
+    return old.union(new)
+
+
+def worker(filename, metadata, mutation_df):
     print("Entered file", filename)
-    # old_chr = ''
-    for index, row in df.iterrows():
-        # print("working on ", filename, index, row['chr'], row['start'])
-        cp = read_file(filename, row['chr'])
-        # if row['chr'] != old_chr:
-        tree = make_interval_tree(cp)
-        interactions, chains = find_interactions(row['start'], tree)
-        df.at[index, int_col] = row[int_col] + interactions
-        df.at[index, chains_col] = row[chains_col] + chains
-        # print("working on ", index, "with", interactions, chains)       
-        # old_chr = row['chr']
-    df.to_csv('Results/processed_' + filename.replace('bedpe', 'csv'), index = False)
-    finish_time = time.perf_counter()
-    print("Elapsed time for" + filename + " in seconds:", finish_time-start_time)
-    return df
+    filename = filename.replace(".bedpe", "")
+    biotype = metadata[metadata["File accession"] == filename]["Experiment target"].values[0].\
+            replace("-human", "") + "_interactions"
+    # read bedpe file as interactions
+    interactions = read_files(filename)
+    
+    for chromosome in np.intersect1d(interactions["CHR1"], "chr" + mutation_df["chr"]):
+        print(chromosome)
+        # filter interactions dataframe to interactions array
+        filtered_interactions, indexStop = filter_bedpe_file(interactions, chromosome)
+        
+        # create IntervalTree
+        tree = create_tree(filtered_interactions)
+        
+        # merge intervals which overlaps directly (not same as merge_overlaps())
+        m_tree = merge_direct_overlaps(tree.copy(), data_reducer=reducer)
+        
+        # convert tree to array
+        array = tree_to_array(m_tree, indexStop)
+        for mutation in mutation_df[ mutation_df["chr"] == chromosome[3:] ]["start"]:
+            interaction_number = build_tree(array, tree.at(mutation))
+            if interaction_number > 0:
+                #print(interaction_number)
+                mutation_df.loc[ (mutation_df["chr"] == chromosome[3:]) & \
+                        (mutation_df["start"] == mutation), biotype ] = interaction_number
+        mutation_df.to_csv(f"Results/processed_{os.path.basename(filename)}.csv", index=False)
 
+   
 if __name__ == '__main__':
-    pool = multiprocessing.Pool()
+    pool = multiprocessing.Pool(processes=os.cpu_count())
     
     FILENAME = "test_data_final.csv"    # change this according to your dataset; make sure to include the file extension
     
@@ -138,9 +185,7 @@ if __name__ == '__main__':
     df.reset_index(drop = True, inplace = True)
 
     df['CTCF_interactions'] = 0
-    df['CTCF_chains'] = 0
     df['POLR2A_interactions'] = 0
-    df['POLR2A_chains'] = 0
 
     jobs = []
     
@@ -150,7 +195,7 @@ if __name__ == '__main__':
     for file in all_files:
         # worker(file, metadata, df)
         print("Starting file", file)
-        jobs.append(pool.apply_async(worker, args=(file, metadata, df,)))
+        jobs.append(pool.apply_async(worker, args=(file, metadata, df.copy(),)))
         # print(result.get(timeout=1))
     pool.close()
     pool.join()
